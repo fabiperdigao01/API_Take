1d7e5478142043a2bff959647c3618074805236d
Fabiana <95590229+fabiperdigao01@users.noreply.github.com>
2021-12-17 21:18:47 -0300

    API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-var http = require("http");
+// create an express app
+const fetch = require("isomorphic-fetch");
+const express = require("express");
+const app = express();
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// define the first route
+app.get("/", async (req, res) => {
+  const url =
+    "https://api.github.com/search/repositories?q=user:takenet+language:csharp&sort=updated&order=asc";
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // SUA LOGICA PODE FICAR AQUI
+      res.send(data);
+    });
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
